128eae9389ecb2c3c7a14f3433bd8809a681366d
Lucas Nog <lucazim.vtk@gmail.com>
2025-06-17 12:06:43 -0300

    exe

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,22 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Post from '.';
-import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+describe('PostComments', () => {
+    it('deve adicionar dois comentários na lista', () => {
+    render(<Post />);
+
+    const input = screen.getByTestId('comment-input') as HTMLTextAreaElement;
+    const button = screen.getByTestId('comment-button');
+
+    fireEvent.change(input, { target: { value: 'Primeiro comentário' } });
+    fireEvent.click(button);
+
+    fireEvent.change(input, { target: { value: 'Segundo comentário' } });
+    fireEvent.click(button);
+
+    const comments = screen.getAllByTestId('comment');
+    expect(comments).toHaveLength(2);
+    expect(comments[0]).toHaveTextContent('Primeiro comentário');
+    expect(comments[1]).toHaveTextContent('Segundo comentário');
+  });
 });
